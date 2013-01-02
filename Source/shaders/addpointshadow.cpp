7926c4db9fbcf2172f6899280a2c418683029506
Lars Pensjo <lars.pensjo@gmail.com>
2013-01-02 19:48:05 +0100

    Improve the comments on point shadows.

--- Source/shaders/addpointshadow.cpp
+++ Source/shaders/addpointshadow.cpp
@@ -32,20 +32,20 @@
 static const GLchar *vertexShaderSource[] = {
 	"#version 330\n", // This corresponds to OpenGL 3.3
 	UNIFORMBUFFER
-	"uniform vec4 Upoint;\n",            // A light. .xyz is the coordinate, and .w is the strength
+	"uniform vec4 Upoint;\n",            // A light. .xyz is the coordinate, and .w is the strength (radius)
 	"layout (location = 0) in vec2 vertex;\n",
 	"out vec2 screen;\n",             // Screen coordinate
 	"void main(void)\n",
 	"{\n",
-	"	float strength = Upoint.w;"
+	"	float radius = Upoint.w;" // Interpreted as the radius of the shadow
 	// Relative bounding (2D) box around the point light
-	"	vec3 box =  vec3(vertex*2-1, 0)*strength;"
+	"	vec3 box =  vec3(vertex*2-1, 0)*radius;"
 	"	vec4 viewPos = UBOViewMatrix * vec4(Upoint.xyz, 1);"
 	"	vec3 d = normalize(viewPos.xyz);"
 	// We want to move the quad towards the player. It shall be moved so as
 	// precisely be outside the range of the lamp. This is needed as the depth
 	// culling will remove parts of the quad that are hidden.
-	"	float l = min(strength, -viewPos.z-1);"
+	"	float l = min(radius, -viewPos.z-1);"
 	// The modelView is one of the corners of the quad in view space.
 	"	vec4 modelView = -vec4(d, 0)*l + vec4(box, 0) + viewPos;"
 	"	vec4 pos = UBOProjectionMatrix * modelView;"
@@ -61,7 +61,7 @@ static const GLchar *fragmentShaderSource[] = {
 	"#version 330\n", // This corresponds to OpenGL 3.3
 	"uniform sampler2D posTex;\n",     // World position
 	"uniform sampler2D normalTex;\n",  // Normals
-	"uniform vec4 Upoint;\n",          // A point source. .xyz is the coordinate, and .w is the strength of the shadow
+	"uniform vec4 Upoint;\n",          // A point source. .xyz is the coordinate, and .w is the radius of the shadow
 	"uniform bool Uselection = false;" // Special case, use a selection color instead of a shadow
 	"in vec2 screen;\n",               // The screen position
 	"layout (location = 0) out vec4 colorChange;\n",
@@ -73,13 +73,13 @@ static const GLchar *fragmentShaderSource[] = {
 	"	vec4 worldPos = texture(posTex, screen);\n",
 	"	float dist = distance(worldPos.xyz, Upoint.xyz);\n",     // Distance to the player or monster
 	"	if (worldPos.y - Upoint.y > 0.3) { discard; return; }"   // Don't draw shadow too high on the object itself.
-	"	float lim = Upoint.w;\n",                                // The maximum distance is coded in the w channel
-	"	if (dist >= lim) {discard; return; }",                   // Is the pixel near, below the player/monster?
+	"	float radius = Upoint.w;\n",                             // The maximum distance (radius of the shadow) is coded in the w channel
+	"	if (dist >= radius) {discard; return; }",                // Is the pixel near, below the player/monster?
 	"	if (Uselection) {"
-	"		colorChange = vec4(1, 0, 0, 0.3);"                   // Shall be used as a blending component
+	"		colorChange = vec4(1, 0, 0, 0.3);"                   // Shall be used as a blending component, adding a red marker at the feet of a monster.
 	"	} else {"
-	"		float f = 1 - (1 - dist/lim)/1.1;\n",
-	"		colorChange = vec4(f, f, f, 1);"                     // Shall be used as a multiplicative component
+	"		float f = 1 - (1 - dist/radius)/1.1;\n",             // 'f' goes from 0.09 to 1.0, depending on distance.
+	"		colorChange = vec4(f, f, f, 1);"                     // Will be used as a multiplicative component, making center almost black.
 	"	}"
 	"}\n",
 };
