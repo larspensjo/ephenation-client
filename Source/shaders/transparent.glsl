8cf8cd14ac5c3a4e15ad70e1c395324ed0c707f0
Lars Pensjo <lars.pensjo@gmail.com>
2013-12-23 13:07:00 +0100

    Move the simplex noise to comon.glsl.

--- Source/shaders/transparent.glsl
+++ Source/shaders/transparent.glsl
@@ -49,30 +49,6 @@ in vec3 position;       // The model coordinate, as given by the vertex shader
 
 layout(location = 0) out vec4 blendOutput;
 
-vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
-vec2 mod289(vec2 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
-vec3 permute(vec3 x) { return mod289(((x*34.0)+1.0)*x); }
-/// An implementation of a 2D simplex noise
-float snoise(vec2 v) {
-	const vec4 C = vec4(0.211324865405187, 0.366025403784439, -0.577350269189626, 0.024390243902439);
-	vec2 i  = floor(v + dot(v, C.yy) );
-	vec2 x0 = v -   i + dot(i, C.xx);
-	vec2 i1; i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
-	vec4 x12 = x0.xyxy + C.xxzz;
-	x12.xy -= i1;
-	i = mod289(i);
-	vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 )) + i.x + vec3(0.0, i1.x, 1.0 ));
-	vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
-	m = m*m ; m = m*m ;
-	vec3 x = 2.0 * fract(p * C.www) - 1.0;
-	vec3 h = abs(x) - 0.5;
-	vec3 ox = floor(x + 0.5);
-	vec3 a0 = x - ox;
-	m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
-	vec3 g; g.x  = a0.x  * x0.x  + h.x  * x0.y; g.yz = a0.yz * x12.xz + h.yz * x12.yw;
-	return 130.0 * dot(m, g);
-}
-
 float height(vec3 pos) {
 	float wave1 = snoise(vec2(pos.x/3, pos.z+time/2.5))/10;
 	float wave2 = snoise(vec2(pos.x/3+1000, pos.z+time/2.5))/10;
